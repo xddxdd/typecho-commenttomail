d547b571215d929949051c1223701d2997d5238b
Lan Tian <xuyh0120@outlook.com>
2017-09-26 01:04:56 +0800

    Fix functionality

--- CommentToMail/Plugin.php
+++ CommentToMail/Plugin.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * 基于 Mailgun 的评论邮件提醒插件，由 CommentGun 改成，原作者 Byends Upd. (http://www.byends.com) & DEFE (http://defe.me)
+ * CommentToMail 升级版，原作者 Byends Upd. (http://www.byends.com) & DEFE (http://defe.me)
  *
- * @package CommentGun
+ * @package CommentToMail
  * @author Lan Tian
  * @version 1.0.0
  * @link https://lantian.pub
  * @oriAuthor Byends Upd. (http://www.byends.com) & DEFE (http://defe.me)
  *
  */
-class CommentGun_Plugin implements Typecho_Plugin_Interface
+class CommentToMail_Plugin implements Typecho_Plugin_Interface
 {
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
@@ -24,8 +24,8 @@ class CommentGun_Plugin implements Typecho_Plugin_Interface
             throw new Typecho_Plugin_Exception(_t('对不起, 您的主机不支持 php-curl 扩展, 无法正常使用此功能'));
         }
         
-        Typecho_Plugin::factory('Widget_Feedback')->finishComment = array('CommentGun_Plugin', 'parseComment');
-        Typecho_Plugin::factory('Widget_Comments_Edit')->finishComment = array('CommentGun_Plugin', 'parseComment');
+        Typecho_Plugin::factory('Widget_Feedback')->finishComment = array('CommentToMail_Plugin', 'parseComment');
+        Typecho_Plugin::factory('Widget_Comments_Edit')->finishComment = array('CommentToMail_Plugin', 'parseComment');
 
         return _t('请设置 API 信息，以使插件正常使用！');
     }
@@ -49,17 +49,43 @@ class CommentGun_Plugin implements Typecho_Plugin_Interface
      */
     public static function config(Typecho_Widget_Helper_Form $form)
     {
+        $mode= new Typecho_Widget_Helper_Form_Element_Radio('mode',
+                array( 'smtp' => 'smtp',
+                       'mail' => 'mail()',
+                       'sendmail' => 'sendmail()'),
+                'smtp', '发信方式');
+        $form->addInput($mode);
 
-        $apikey = new Typecho_Widget_Helper_Form_Element_Text('apikey', NULL, 'key-',
-                _t('API KEY'), _t('请填写 API KEY'));
-        $form->addInput($apikey->addRule('required', _t('必须填写一个 API KEY')));
+        $host = new Typecho_Widget_Helper_Form_Element_Text('host', NULL, 'smtp.',
+                _t('SMTP地址'), _t('请填写 SMTP 服务器地址'));
+        $form->addInput($host->addRule('required', _t('必须填写一个SMTP服务器地址')));
 
-        $apidomain = new Typecho_Widget_Helper_Form_Element_Text('apidomain', NULL, '',
-                _t('API 域名'), _t('请填写发信域名'));
-        $form->addInput($apidomain->addRule('required', _t('必须填写发信域名')));
+        $port = new Typecho_Widget_Helper_Form_Element_Text('port', NULL, '25',
+                _t('SMTP端口'), _t('SMTP服务端口,一般为25。'));
+        $port->input->setAttribute('class', 'mini');
+        $form->addInput($port->addRule('required', _t('必须填写SMTP服务端口'))
+                ->addRule('isInteger', _t('端口号必须是纯数字')));
+
+        $user = new Typecho_Widget_Helper_Form_Element_Text('user', NULL, NULL,
+                _t('SMTP用户'),_t('SMTP服务验证用户名,一般为邮箱名如：youname@domain.com'));
+        $form->addInput($user->addRule('required', _t('SMTP服务验证用户名')));
+
+        $pass = new Typecho_Widget_Helper_Form_Element_Password('pass', NULL, NULL,
+                _t('SMTP密码'));
+        $form->addInput($pass->addRule('required', _t('SMTP服务验证密码')));
+
+        $validate = new Typecho_Widget_Helper_Form_Element_Checkbox('validate',
+                array('validate'=>'服务器需要验证',
+                    'ssl'=>'ssl加密'),
+                array('validate'),'SMTP验证');
+        $form->addInput($validate);
+        
+        $fromName = new Typecho_Widget_Helper_Form_Element_Text('fromName', NULL, NULL,
+                _t('发件人名称'),_t('发件人名称，留空则使用博客标题'));
+        $form->addInput($fromName);
 
         $mail = new Typecho_Widget_Helper_Form_Element_Text('mail', NULL, NULL,
-                _t('接收邮件的地址'),_t('接收邮件的地址，如为空则使用文章作者个人设置中的邮件地址！'));
+                _t('接收邮件的地址'),_t('接收邮件的地址,如为空则使用文章作者个人设置中的邮件地址！'));
         $form->addInput($mail->addRule('email', _t('请填写正确的邮件地址！')));
 
         $contactme = new Typecho_Widget_Helper_Form_Element_Text('contactme', NULL, NULL,
@@ -80,11 +106,11 @@ class CommentGun_Plugin implements Typecho_Plugin_Interface
                 array('to_owner','to_guest'), '其他设置','');
         $form->addInput($other->multiMode());
 
-        $titleForOwner = new Typecho_Widget_Helper_Form_Element_Text('titleForOwner',null,"《{title}》一文有新的评论",
+        $titleForOwner = new Typecho_Widget_Helper_Form_Element_Text('titleForOwner',null,"[{title}] 一文有新的评论",
                 _t('博主接收邮件标题'));
         $form->addInput($titleForOwner->addRule('required', _t('博主接收邮件标题 不能为空')));
 
-        $titleForGuest = new Typecho_Widget_Helper_Form_Element_Text('titleForGuest',null,"您在《{title}》的评论有了回复",
+        $titleForGuest = new Typecho_Widget_Helper_Form_Element_Text('titleForGuest',null,"您在 [{title}] 的评论有了回复",
                 _t('访客接收邮件标题'));
         $form->addInput($titleForGuest->addRule('required', _t('访客接收邮件标题 不能为空')));
     }
@@ -110,7 +136,7 @@ class CommentGun_Plugin implements Typecho_Plugin_Interface
         $db = Typecho_Db::get();
         $user = Typecho_Widget::widget('Widget_User');
         $options = Typecho_Widget::widget('Widget_Options');
-        $cfg = $options->plugin('CommentGun');
+        $cfg = $options->plugin('CommentToMail');
         $email = array(
             'siteTitle' => $options->title,
             'timezone'  => $options->timezone,
@@ -265,22 +291,63 @@ class CommentGun_Plugin implements Typecho_Plugin_Interface
      */
     public function sendMail($email)
     {
-        $cfg = Typecho_Widget::widget('Widget_Options')->plugin('CommentGun');
-        $apiAddress = 'https://api.mailgun.net/v3/' . $cfg->apidomain . '/messages';
-        $data = array(
-            'from' => 'noreply@' . $cfg->apidomain,
-            'to' => $email['toName'] . " <" . $email['to'] . ">",
-            'subject' => $email['subject'],
-            'html' => $email['msgHtml']
-        );
+        /** 载入邮件组件 */
+        require __DIR__ . '/lib/class.phpmailer.php';
+        require __DIR__ . '/lib/class.smtp.php';
 
-        $ch = curl_init($apiAddress);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_USERPWD, 'api:' . $cfg->apikey);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $result = curl_exec($ch);
-        curl_close($ch);
+        $cfg = Typecho_Widget::widget('Widget_Options')->plugin('CommentToMail');
+
+        $mailer = new PHPMailer();
+        $mailer->CharSet = 'UTF-8';
+        $mailer->Encoding = 'base64';
+
+        //选择发信模式
+        switch ($cfg->mode)
+        {
+            case 'mail':
+                break;
+            case 'sendmail':
+                $mailer->IsSendmail();
+                break;
+            case 'smtp':
+                $mailer->IsSMTP();
+
+                if (in_array('validate', $cfg->validate)) {
+                    $mailer->SMTPAuth = true;
+                }
+
+                if (in_array('ssl', $cfg->validate)) {
+                    $mailer->SMTPSecure = "ssl";
+                }
+
+                $mailer->Host     = $cfg->host;
+                $mailer->Port     = $cfg->port;
+                $mailer->Username = $cfg->user;
+                $mailer->Password = $cfg->pass;
+
+                break;
+        }
+
+        $mailer->SetFrom($email['from'], $email['fromName']);
+        $mailer->AddReplyTo($email['to'], $email['toName']);
+        $mailer->Subject = $email['subject'];
+        $mailer->AltBody = $email['altBody'];
+        $mailer->MsgHTML($email['msgHtml']);
+        $mailer->AddAddress($email['to'], $email['toName']);
+      
+        $mailer->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        $result = $mailer->Send();
+        if (!$result) $result = $mailer->ErrorInfo;
+        
+        $mailer->ClearAddresses();
+        $mailer->ClearReplyTos();
+
         return $result;
     }
 
--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-# Typecho-CommentGun
-A plugin for typecho to notify readers of new comments by sending emails via Mailgun.
+# Typecho-CommentToMail
+A plugin for typecho to notify readers of new comments by sending emails via SMTP.
 
 This plugin is modified from [CommentToMail](https://github.com/byends/CommentToMail).
 The original author of CommentToMail is [Byends Upd.](http://www.byends.com).
 The original original author is [DEFE](http://defe.me).
 
 ## Modifications
-1. Changed sending method from Sendmail/SMTP to Mailgun API.
-2. Removed async sending - Mailgun API is too fast to cause lag.
+2. Removed async sending, as with modern SSL it causes way too many bugs.
 3. Removed logging and caching - these causes unnecessary disk operations, which may cause lag on some systems.
 
 ## Installation
-Just drop the *CommentGun* folder into *usr/plugins/* folder under your Typecho installation, and enter your API key and domain into the control panel.
+Just drop the *CommentToMail* folder into *usr/plugins/* folder under your Typecho installation, and enter your SMTP information into the control panel.
